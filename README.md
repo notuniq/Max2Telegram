1f68227eecfbe53fbedd0d1b4caa85b2607a85e7
uni <87239155+notuniq@users.noreply.github.com>
2026-03-14 23:59:52 +0300

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # Max2Telegram
-Бот для пересылки сообщений из чата MAX в Telegram.
+Бот для пересылки сообщений из чата **MAX** в **Telegram**.  
+
+---
+
+Функционал пересылки:
+- Текстовые сообщения  
+- Фото (несколько фото объединяются в одно сообщение)  
+- Файлы (с текстом как подпись)  
+- Объединение текста + фото или текста + файла в одном сообщении  
+
+---
+
+## 🚀 Установка
+
+1. Клонируйте репозиторий:  
+
+```bash
+git clone https://github.com/notuniq/Max2Telegram.git
+cd Max2Telegram
+```
+2. Создайте виртуальное окружение и установите зависимости:
+```bash
+python -m venv venv
+# Linux/macOS
+source venv/bin/activate
+# Windows
+venv\Scripts\activate
+pip install -r requirements.txt
+```
+3. Создайте файл .env с вашими настройками:
+```
+MAX_AUTH_TOKEN=<Ваш токен из MAX>
+MAX_CHAT_ID=<ID чата MAX>
+TG_TOKEN=<Telegram Bot Token>
+TG_CHAT_ID=<ID чата Telegram>
+```
+
+## Получение MAX_AUTH_TOKEN
+
+1. Перейдите в [веб-версию MAX](https://web.max.ru)
+2. Откройте DevTools (F12 или Ctrl+Shift+I)
+3. Перейдите на вкладку Application → Local Storage
+4. Найдите ключ __oneme_auth
+5. Скопируйте значение token и вставьте его в .env как MAX_AUTH_TOKEN
+
+## Получение MAX_CHAT_ID
+1. Зайдите в ваш чат через [веб-версию MAX](https://web.max.ru)
+2. Скопируйте ссылку на чат в адресной строке браузера
+
+```
+https://web.max.ru/-12345678
+```
+3. Скопируйте из ссылки -12345678 и вставьте в MAX_CHAT_ID
+
+## Получение TG_TOKEN
+
+1. Создайте бота с помощью [BotFather](https://t.me/BotFather)
+2. После создания скопируйте токен бота и вставьте его в TG_TOKEN
+```
+Use this token to access the HTTP API:
+12345678:ADdaJLKAAdjalJDeadjaoiIIP
+Keep your token secure and store it safely, it can be used by anyone to control your bot.
+```
+
+## [Получение TG_CHAT_ID (клик)](https://habr.com/ru/companies/amvera/articles/996686/)
+
+## Запуск бота
+```bash
+python src/main.py
+```
+Бот подключится к MAX, будет проверять новые сообщения и пересылать их в Telegram.
+
+## Лицензия
+Проект лицензирован под MIT License.
+
+MIT License позволяет:
+- Свободно использовать код
+- Форкать и дорабатывать
+- Распространять любые изменения
+
+```
+Сохранение лицензии обязательно при любых изменениях или распространении.
+```
---
+++ src/main.py
@@ -0,0 +1,197 @@
+import asyncio
+import json
+import os
+from uuid import uuid4
+from httpx import AsyncClient
+from websockets import Origin
+from websockets.asyncio.client import connect
+import logging
+from dotenv import load_dotenv
+import html
+
+from models.max import BaseMaxApiModel, MaxAuthTokenRequest, MaxUserAgent, MaxTokenData, MaxGetMessagesRequest, MaxGetMessagesRequestPayload, MaxGetFileUrlPayload, MaxGetFileUrlRequest
+
+load_dotenv()
+
+logging.basicConfig(
+    format="%(asctime)s %(message)s",
+    level=logging.DEBUG,
+)
+
+def get_last_msg_id():
+    try:
+        content = open("src/last_msg_id.txt", "r+").readline().strip()
+        return content if content else None
+    except FileNotFoundError:
+        return None
+
+def set_last_msg_id(msg_id):
+    with open("src/last_msg_id.txt", "w") as f:
+        f.write(str(msg_id))
+
+
+async def max_connect():
+    url = "wss://ws-api.oneme.ru/websocket"
+    last_message_id = get_last_msg_id()
+    seq = 0
+
+    async with connect(
+        url,
+        user_agent_header=os.getenv("MAX_USER_AGENT"),
+        origin=Origin("https://web.max.ru"),
+    ) as ws:
+        initial_session_request = BaseMaxApiModel(
+            cmd=0,
+            ver=11,
+            seq=seq,
+            opcode=6,
+            payload={
+                "deviceId": str(uuid4()),
+                "userAgent": MaxUserAgent(
+                    deviceType=os.getenv("MAX_DEVICE_TYPE"),
+                    locale=os.getenv("MAX_LOCALE"),
+                    deviceLocale=os.getenv("MAX_DEVICE_LOCALE"),
+                    osVersion=os.getenv("MAX_OS_VERSION"),
+                    headerUserAgent=os.getenv("MAX_USER_AGENT"),
+                    appVersion=os.getenv("MAX_APP_VERSION"),
+                    screen=os.getenv("MAX_SCREEN"),
+                    timezone=os.getenv("MAX_TZ"),
+                    deviceName=os.getenv("MAX_DEVICE_NAME")
+                ).model_dump(),
+            },
+        )
+        await ws.send(json.dumps(initial_session_request.model_dump()))
+        seq += 1
+
+        res = BaseMaxApiModel(**json.loads(await ws.recv()))
+        logging.info(f"Initial session established: opcode={res.opcode}")
+
+        auth_request = MaxAuthTokenRequest(
+            seq=seq,
+            payload=MaxTokenData(
+                interactive=True,
+                token=os.getenv("MAX_AUTH_TOKEN"),
+                chatsCount=40,
+                chatsSync=0,
+                contactsSync=0,
+                presenceSync=0,
+                draftsSync=0
+            )
+        )
+        await ws.send(json.dumps(auth_request.model_dump()))
+        seq += 1
+
+        res = BaseMaxApiModel(**json.loads(await ws.recv()))
+        logging.info(f"Авторизация успешна: opcode={res.opcode}")
+
+        logging.info("Запускаю loop сообщений...")
+
+        client = AsyncClient(base_url=f"https://api.telegram.org/bot{os.getenv('TG_TOKEN')}")
+        
+        while True:
+            get_messages_request = MaxGetMessagesRequest(
+                seq=seq,
+                payload=MaxGetMessagesRequestPayload(
+                    chatId=int(os.getenv("MAX_CHAT_ID")),
+                    forward=0,
+                    backward=30,
+                    getMessages=True,
+                )
+            )
+            await ws.send(json.dumps(get_messages_request.model_dump(by_alias=True)))
+            seq += 1
+
+            res = json.loads(await ws.recv())
+            messages = res.get("payload", {}).get("messages", [])
+
+            if not messages:
+                logging.debug("Нет новых сообщений")
+                await asyncio.sleep(3)
+                continue
+
+            last_message = messages[-1]
+            current_msg_id = str(last_message.get("id", ""))
+
+            if last_message.get("link") and last_message["link"]["type"] == "FORWARD":
+                last_message = last_message["link"]["message"]
+
+            if current_msg_id == last_message_id:
+                logging.debug("Нет новых сообщений")
+                await asyncio.sleep(3)
+                continue
+
+            logging.info(f"Новое сообщение! ID: {current_msg_id}")
+            set_last_msg_id(current_msg_id)
+            last_message_id = current_msg_id
+
+            text = last_message.get("text", "").strip()
+            escaped_text = html.escape(text) if text else None
+
+            photos = [
+                {"type": "photo", "media": attach["baseUrl"]}
+                for attach in last_message.get("attaches", [])
+                if attach["_type"] == "PHOTO"
+            ]
+
+            files = [
+                attach
+                for attach in last_message.get("attaches", [])
+                if attach["_type"] == "FILE"
+            ]
+
+            if photos:
+                if escaped_text:
+                    photos[0]["caption"] = escaped_text
+                    photos[0]["parse_mode"] = "html"
+                await client.post("/sendMediaGroup", json={
+                    "chat_id": os.getenv("TG_CHAT_ID"),
+                    "media": photos
+                })
+
+            elif files:
+                for attach in files:
+                    file_id = attach["fileId"]
+                    request = MaxGetFileUrlRequest(
+                        seq=seq,
+                        payload=MaxGetFileUrlPayload(
+                            fileId=file_id,
+                            chatId=int(os.getenv("MAX_CHAT_ID")),
+                            messageId=last_message["id"]
+                        )
+                    )
+                    await ws.send(json.dumps(request.model_dump()))
+                    seq += 1
+                    res_file = json.loads(await ws.recv())
+                    file_url = res_file.get("payload", {}).get("url")
+                    if not file_url:
+                        logging.warning("URL файла не получен")
+                        continue
+
+                    async with AsyncClient() as http:
+                        file_data = await http.get(file_url)
+
+                    await client.post(
+                        "/sendDocument",
+                        data={
+                            "chat_id": os.getenv("TG_CHAT_ID"),
+                            "caption": escaped_text or attach["name"]
+                        },
+                        files={"document": (attach["name"], file_data.content)}
+                    )
+
+            elif escaped_text:
+                await client.post("/sendMessage", json={
+                    "chat_id": os.getenv("TG_CHAT_ID"),
+                    "text": f"{escaped_text}",
+                    "parse_mode": "html"
+                })
+
+            await asyncio.sleep(3)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(max_connect())
+    except KeyboardInterrupt:
+        logging.info("Приложение остановлено")
+    except Exception as e:
+        logging.error(f"Ошибка приложения: {e}")
